4329c2a5d86ff1d6394efbb98f2b5a3e9829b0eb
= <=>
2024-08-24 22:47:11 +0000

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,10 +10,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage", strict_slashes=False)
 def hello():
     """Displays 'Hello HBNB!'"""
-    return render_template("10-hbnb_filters.py")
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
