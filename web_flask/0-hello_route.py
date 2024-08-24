c3848f11c713aed71fd7b07d6d470b71b4bd544a
= <=>
2024-08-24 22:34:38 +0000

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,17 +5,17 @@ The application listen on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
+def hello():
     """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    return render_template("10-hbnb_filters.py")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000, debug=None)
 
